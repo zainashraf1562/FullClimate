bd892894d0be9505d611e0d4d7a464aafd376875
zainashraf1562 <zainashraf1562@gmail.com>
2024-02-14 12:07:21 -0500

    Initial Commit

---
+++ FullClimate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FullClimate
+//
+//  Created by Noman Ashraf on 2/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
